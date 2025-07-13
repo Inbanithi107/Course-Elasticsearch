21681033bf182ad0ee6993f11b461f2120f88ef4
Inbanithi <inbanithi107@gmail.com>
2025-07-13 17:46:17 +0530

    initial app setup and elasticsearch with docker

---
+++ src/main/java/com/Techforge/aop/CourseElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.Techforge.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseElasticSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseElasticSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Techforge/aop/CourseElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Techforge.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseElasticSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
